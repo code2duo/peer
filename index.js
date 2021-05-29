3ca9900accf8601f93160df6d2c909e80fc9c16f
Debdut Goswami <debdutgoswami@gmail.com>
2021-05-30 03:18:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const { PeerServer } = require('peer');
 
-const peerServer = PeerServer({ port: 5000, path: '/' });
+const PORT = process.env.PORT || 5000;
+
+const peerServer = PeerServer({ port: PORT, path: '/' });
 
 peerServer.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-const PORT = process.env.PORT || 5000;
-
-peerServer.listen(PORT, '0.0.0.0', () => console.log(`Server listening on port ${PORT}`))
